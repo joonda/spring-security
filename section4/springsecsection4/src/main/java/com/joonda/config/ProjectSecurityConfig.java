d83cd69f6d4c621128a8de836f23483af483a163
joonda <kloudzero@kakao.com>
2025-07-22 23:01:05 +0900

    :sparkles: [Feat] Section4 complete

--- section4/springsecsection4/src/main/java/com/joonda/config/ProjectSecurityConfig.java
+++ section4/springsecsection4/src/main/java/com/joonda/config/ProjectSecurityConfig.java
@@ -24,14 +24,15 @@ public class ProjectSecurityConfig {
         // denyAll -> 인증된 사용자든 익명 사용자든 관계 없이 API로 들어오는 모든 request를 거부 (403 에러 반환)
         // http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
         // http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
-        http.authorizeHttpRequests((requests) -> requests
+        http.csrf(csrfConfig -> csrfConfig.disable())
+          .authorizeHttpRequests((requests) -> requests
                 // 로그인이 필요한 페이지는 보안을 설정하고, 로그인이 필요없는 페이지는 permitAll로 보안을 해지.
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                 // 기본적으로 Spring Security가 런타임 에러가 발생하면 error 페이지로 리디렉션한다
                 // 해당 error 페이지는 기본적으로 authenticated 상태이기 때문에, permitAll 로 설정해줘야 오류 메시지를 볼 수 있다.
                 // 상황에 따라서 error 페이지를 authenticated로 할지, permitAll로 할지는 선택!
                 // 어디에도 명시를 하지 않으면, 기본적으로 Spring Security 프레임워크는 항상 403 오류 발생
-                .requestMatchers("/notices", "/contact", "/error").permitAll());
+                .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
