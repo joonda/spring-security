9e01bf21181116e662f87457e7d879e5dd59ec3b
joonda <kloudzero@kakao.com>
2025-08-10 16:05:47 +0900

    :sparkles: demo pull 2

--- section7/eazyschool-end/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ section7/eazyschool-end/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -1,8 +1,5 @@
 package com.eazybytes.eazyschool.config;
 
-import com.eazybytes.eazyschool.handler.CustomAuthenticationFailureHandler;
-import com.eazybytes.eazyschool.handler.CustomAuthenticationSuccessHandler;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
@@ -18,26 +15,17 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
-@RequiredArgsConstructor
 public class ProjectSecurityConfig {
-
-    private final CustomAuthenticationSuccessHandler authenticationSuccessHandler;
-    private final CustomAuthenticationFailureHandler authenticationFailureHandler;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf((csrf) -> csrf.disable())
-                .authorizeHttpRequests((requests) -> requests.requestMatchers("/dashboard").authenticated()
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/dashboard").permitAll()
                         .requestMatchers("/", "/home", "/holidays/**", "/contact", "/saveMsg",
-                                "/courses", "/about", "/assets/**", "/login/**").permitAll())
-                .formLogin(flc -> flc.loginPage("/login").usernameParameter("userid").passwordParameter("secretPwd")
-                        .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true")
-                        .successHandler(authenticationSuccessHandler).failureHandler(authenticationFailureHandler))
-                .logout(loc -> loc.logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).clearAuthentication(true)
-                        .deleteCookies("JSESSIONID"))
+                                "/courses", "/about", "/assets/**").permitAll())
+                .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
-
 
         return http.build();
     }
--- section7/eazyschool-end/src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
+++ section7/eazyschool-end/src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
@@ -2,7 +2,6 @@ package com.eazybytes.eazyschool.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DashboardController {
 
     @RequestMapping("/dashboard")
-    public String displayDashboard(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    public String displayDashboard(Model model,Authentication authentication) {
         if(null != authentication) {
             model.addAttribute("username", authentication.getName());
             model.addAttribute("roles", authentication.getAuthorities().toString());
